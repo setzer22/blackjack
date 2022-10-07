c3367890ffd380071ec9395b6ce293995d23f938
Setzer22 <jsanchezfsms@gmail.com>
2022-10-07 18:26:02 +0200

    License boilerplate

--- blackjack_macros/src/lib.rs
+++ blackjack_macros/src/lib.rs
@@ -1,3 +1,9 @@
+// Copyright (C) 2022 setzer22 and contributors
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use syn::{parse_macro_input, ItemMod};
 
 mod blackjack_lua_module;
