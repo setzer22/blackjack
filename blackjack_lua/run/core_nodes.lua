fcf102d274ceaaf077950b058c70e757d00e5119
Zireael07 <zi@mail.com>
2022-11-09 14:25:25 +0100

    expose divide edge as a node

--- blackjack_lua/run/core_nodes.lua
+++ blackjack_lua/run/core_nodes.lua
@@ -394,6 +394,23 @@ local edit_ops = {
             end
         end,
     },
+    SubdivideEdge = {
+        label = "Subdivide Edge",
+        inputs = { 
+            P.mesh("in_mesh"),
+            P.selection("edge"),
+            P.scalar("interp", { default = 0.5, soft_min = 0.0, soft_max = 1.0 }),
+        },
+        outputs = {
+            P.mesh("out_mesh"),
+        },
+        returns = "out_mesh",
+        op = function(inputs)
+            local out_mesh = inputs.in_mesh:clone()
+            Ops.subdivide_edge(out_mesh, inputs.edge, inputs.interp)
+            return { out_mesh = out_mesh }
+        end,
+    },
     SetNormals = {
         label = "Set Normals",
         inputs = {
