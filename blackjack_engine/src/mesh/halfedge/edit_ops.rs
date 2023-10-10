c3ac55c37915debf08a7b8549b6f15250a8fbdbe
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-10-09 20:40:02 -0400

    Progress
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- blackjack_engine/src/mesh/halfedge/edit_ops.rs
+++ blackjack_engine/src/mesh/halfedge/edit_ops.rs
@@ -1560,7 +1560,7 @@ pub fn extrude_along_curve(
     let mut positions = vec![];
 
     let first_vertex_id = backbone_conn.iter_vertices().next().unwrap().0;
-    println!("first_vertex_id={:?}", first_vertex_id);
+    // println!("first_vertex_id={:?}", first_vertex_id);
     for (v, _) in backbone_conn.iter_vertices() {
         let scale = if let Ok(ref size) = backbone_size {
             Vec3::splat(size[v])
@@ -1574,10 +1574,10 @@ pub fn extrude_along_curve(
             let normal = normal_ch[v];
             let tangent = tangent_ch[v];
             let cotangent = normal.cross(tangent);
-            println!(
-                "v={:?}, normal={:?}, tangent={:?}, cotangent={:?}",
-                v, normal, tangent, cotangent
-            );
+            // println!(
+            //     "v={:?}, normal={:?}, tangent={:?}, cotangent={:?}",
+            //     v, normal, tangent, cotangent
+            // );
             let (_, rotate, _) = glam::Affine3A::from_cols(
                 cotangent.into(),
                 normal.into(),
@@ -1590,13 +1590,19 @@ pub fn extrude_along_curve(
         } else {
             glam::Quat::IDENTITY
         };
-        println!("v={:?}, rotate={:?}", v, rotate);
+        // println!("v={:?}, rotate={:?}", v, rotate);
 
         for vc in csect_chain.iter_cpy() {
             let pos = csect_pos[vc] - backbone_pos[first_vertex_id];
             // let rot = Quat::from_euler(glam::EulerRot::XYZ, rotate.x, rotate.y, rotate.z);
-            println!("v={:?}, pos={:?}, pos*scale={:?}, rotate*(pos*scale)={:?} backbone_pos={:?}, final={:?}",
-            v, pos, pos*scale, rotate*(pos*scale), backbone_pos[v], rotate * (pos * scale) + backbone_pos[v]);
+            // println!(
+            //     "v={:?}, pos={:?}, rotate*(pos*scale)={:?} backbone_pos={:?}, final={:?}",
+            //     v,
+            //     pos,
+            //     rotate * (pos * scale),
+            //     backbone_pos[v],
+            //     rotate * (pos * scale) + backbone_pos[v]
+            // );
             positions.push(rotate * (pos * scale) + backbone_pos[v]);
         }
     }
--- blackjack_lua/run/helix.lua
+++ blackjack_lua/run/helix.lua
@@ -5,39 +5,56 @@ NodeLibrary:addNodes({
     Helix = {
         label = "Helix",
         op = function(inputs)
-	    local points = {}
-	    -- Generate the points
-	    local max_angle = inputs.turns * 2.0 * math.pi
-	    local num_steps = math.ceil(inputs.turns * inputs.segments)
+            local points = {}
+            -- Generate the points
+            local max_angle = inputs.turns * 2.0 * math.pi
+            local num_steps = math.ceil(inputs.turns * inputs.segments)
 
-	    if num_steps < 1 then
-                return { out_mesh = Primitives.line_from_points(points) }
-	    end
+            if num_steps < 1 then
+                return {
+                    out_mesh = Primitives.line_from_points(points)
+                }
+            end
 
-	    local angle_delta = max_angle / num_steps
-	    local delta_y = inputs.size.y * inputs.turns / num_steps
-	    local direction = inputs.direction == "Clockwise" and -1 or 1
-	    local start_angle = math.pi * inputs.start_angle / 180
-	    for i = 0, num_steps do
-	    	local angle = direction * (start_angle + i * angle_delta)
-	        local x = inputs.pos.x + inputs.size.x * math.cos(angle)
-	        local z = inputs.pos.z + inputs.size.z * math.sin(angle)
-	        local y = inputs.pos.y + i * delta_y
-		table.insert(points, vector(x, y, z))
-	    end
-            return { out_mesh = Primitives.line_from_points(points) }
+            local normals = {}
+            local tangents = {}
+            local angle_delta = max_angle / num_steps
+            local delta_y = inputs.size.y * inputs.turns / num_steps
+            local direction = inputs.direction == "Clockwise" and -1 or 1
+            local start_angle = math.pi * inputs.start_angle / 180
+            for i = 0, num_steps do
+                local angle = direction * (start_angle + i * angle_delta)
+                local x = inputs.pos.x + inputs.size.x * math.cos(angle)
+                local z = inputs.pos.z + inputs.size.z * math.sin(angle)
+                local y = inputs.pos.y + i * delta_y -- y is "up"
+                local nx = 0.0
+                local nz = 0.0
+                local ny = 1.0
+                local tx = math.sin(angle)
+                local tz = math.cos(angle)
+                local ty = 0.0
+                table.insert(points, vector(x, y, z))
+                table.insert(normals, vector(nx, ny, nz))
+                table.insert(tangents, vector(tx, ty, tz))
+            end
+            return {
+                out_mesh = Primitives.line_with_normals(points, normals, tangents, num_steps)
+            }
         end,
-        inputs = {
-            P.v3("pos", vector(0, 0, 0)),
-            P.v3("size", vector(1, 1, 1)),
-            P.scalar("start_angle", { default = 0, min = 0, soft_max = 360 }),
-            P.scalar("turns", { default = 1, min = 0, soft_max = 10 }),
-            P.scalar_int("segments", { default = 36, min = 1, soft_max = 360 }),
-	    P.enum("direction", { "Clockwise", "Counter-Clockwise"}, 0),
-        },
-        outputs = {
-            P.mesh("out_mesh"),
-        },
-        returns = "out_mesh",
-    },
+        inputs = {P.v3("pos", vector(0, 0, 0)), P.v3("size", vector(1, 1, 1)), P.scalar("start_angle", {
+            default = 0,
+            min = 0,
+            soft_max = 360
+        }), P.scalar("turns", {
+            default = 1,
+            min = 0,
+            soft_max = 10
+        }), P.scalar_int("segments", {
+            default = 36,
+            min = 1,
+            soft_max = 360
+        }), P.enum("direction", {"Clockwise", "Counter-Clockwise"}, 0)},
+        outputs = {P.mesh("out_mesh")},
+        returns = "out_mesh"
+    }
 })
